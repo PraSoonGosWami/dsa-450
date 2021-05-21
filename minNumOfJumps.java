0d283eaeb5db539c1270074a42348606f75ab248
Prasoon Goswami <mark55@Prasoons-MacBook-Pro.local>
2021-05-22 02:13:52 +0530

    Changed loop end from n to n-1

--- minNumOfJumps.java
+++ minNumOfJumps.java
@@ -6,12 +6,13 @@ public class minNumOfJumps {
      * https://practice.geeksforgeeks.org/problems/minimum-number-of-jumps-1587115620/1
      */
 
+    //{2,3,1,1,4}
     // O(N^2) | O(1)
     public static int solution1(int arr[]) {
         int i = 0;
         int jump = 0;
         int n = arr.length;
-        while (i < n) {
+        while (i < n-1) {
             int maxRange = i + arr[i];
             if (maxRange >= n - 1) return ++jump;
             int index = i + 1;
@@ -35,9 +36,7 @@ public class minNumOfJumps {
         int jump = 1;
         int steps = arr[0];
         int maxJump = arr[0];
-        for(int i = 1; i < arr.length; i++){
-            if(i == arr.length-1)
-                return jump;
+        for(int i = 1; i < arr.length - 1; i++){
             maxJump = Math.max(maxJump, i+arr[i]);
             steps--;
             if(steps == 0){
